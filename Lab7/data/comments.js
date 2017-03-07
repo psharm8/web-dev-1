73b4fe8735175add0e8de825d28f16992d37abce
Puneet Sharma <puneet.s2004@gmail.com>
2017-03-07 15:46:16 -0500

    Completed

---
+++ Lab7/data/comments.js
@@ -0,0 +1,99 @@
+﻿const mongoCollections = require("../config/mongoCollections");
+const recipeData = require("./recipes");
+const recipes = mongoCollections.recipes;
+const uuid = require("node-uuid");
+
+let exportedMethods = {
+    getCommentsByRecipeId(id) {
+        if (!id) {
+            return Promise.reject("You must provide the id of recipe.");
+        }
+        return recipeData.getRecipeById(id)
+            .then(recipe => {
+                return recipe.comments.map(c => {
+                    c["recipeId"] = recipe._id;
+                    c["recipeTitle"] = recipe.title;
+                    return c;
+                });
+            });
+    },
+    getCommentById(id) {
+        if (!id) {
+            return Promise.reject("You must provide the id of comment.");
+        }
+        return recipes().then((collection) => {
+            return collection.findOne({ "comments._id": id })
+                .then(recipe => {
+                    if (!recipe) {
+                        throw "Comment not found";
+                    }
+                    let comment = recipe.comments.find(c => c._id === id);
+                    comment["recipeId"] = recipe._id;
+                    comment["recipeTitle"] = recipe.title;
+                    return comment;
+                });
+        });
+    },
+    addCommentToRecipe(id,poster, comment) {
+        if (!id) {
+            return Promise.reject("You must provide the id of recipe.");
+        }
+        if (!comment) {
+            return Promise.reject("No comment provided.");
+        }
+        return recipeData.getRecipeById(id)
+            .then(() => {
+                let newComment = {
+                    _id: uuid.v4(),
+                    poster: poster,
+                    comment: comment
+                };
+                return recipes().then(collection => {
+                    return collection.updateOne({ _id: id }, { $addToSet: { comments: newComment } })
+                        .then(() => {
+                            return this.getCommentById(newComment._id);
+                        });
+                });
+            });
+    },
+    updateComment(recipeId, commentId, updated) {
+        if (!recipeId) {
+            return Promise.reject("You must provide a recipe id to update.");
+        }
+        if (!commentId) {
+            return Promise.reject("You must provide a comment id to update.");
+        }
+        if (!updated) {
+            return Promise.reject("No update provided.");
+        }
+
+        return this.getCommentById(commentId).then((comment) => {
+            if (comment.recipeId !== recipeId) {
+                throw "This comment does not belong to provided recipe id.";
+            }
+            let update= {}
+            if (updated.poster) {
+                update["comments.$.poster"] = updated.poster;
+            }
+            if (updated.comment) {
+                update["comments.$.comment"] = updated.comment;
+            }
+            return recipes().then(collection => {
+                return collection.updateOne({ "comments._id": commentId }, { $set: update })
+                    .then(() => {
+                        return this.getCommentById(commentId);
+                    });
+            });
+        });
+    },
+    removeComment(id) {
+        if (!id) {
+            return Promise.reject("You must provide a comment id to delete.");
+        }
+        return recipes().then(collection => {
+            return collection.updateOne({ "comments._id": id }, { $pull: { "comments": { _id: id } } });
+        });
+    }
+}
+
+module.exports = exportedMethods;
---
+++ Lab7/routes/recipes.js
@@ -0,0 +1,67 @@
+﻿const express = require('express');
+const router = express.Router();
+const data = require("../data");
+const recipeData = data.recipes;
+
+router.get("/:id",
+    (req, res) => {
+        recipeData.getRecipeById(req.params.id).then(recipe => {
+                res.json(recipe);
+            },
+            () => {
+                res.status(404).json({ error: "Recipie not found" });
+            });
+    });
+
+router.put("/:id",
+    (req, res) => {
+        let updatedData = req.body;
+        recipeData.getRecipeById(req.params.id).then(recipe => {
+            return recipeData.updateRecipe(req.params.id, updatedData)
+                .then(updated => {
+                        res.json(updated);
+                    },
+                    e => {
+                        res.status(500).json({ error: e });
+                    });
+        });
+    });
+
+router.delete("/:id",
+    (req, res) => {
+        recipeData.getRecipeById(req.params.id).then(() => {
+            recipeData.removeRecipe(req.params.id)
+                .then(() => {
+                        res.sendStatus(200);
+                    },
+                    e => {
+                        res.status(500).json({ error: e });
+                    });
+        }).catch(err => {
+            res.status(404).send({ error: err });
+        });
+    });
+
+router.post("/",
+    (req, res) => {
+        let recipe = req.body;
+        recipeData.addRecipe(recipe.title, recipe.ingredients, recipe.steps)
+            .then(newRecipe => {
+                    res.json(newRecipe);
+                },
+                () => {
+                    res.status(500).json({ error: e });
+                });
+    });
+
+router.get("/",
+    (req, res) => {
+        recipeData.getAllRecipes().then(recipes => {
+                res.json(recipes);
+            },
+            e => {
+                res.status(500).json({ error: e });
+            });
+    });
+
+module.exports = router;
